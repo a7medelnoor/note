6763472e41d86836595c20642f8e91842af9322e
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-09-28 10:40:17 +0400

    refactor MainActivity to java

---
+++ app/src/main/java/com/a7medelnoor/note/MainActivity.java
@@ -0,0 +1,13 @@
+package com.a7medelnoor.note;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
