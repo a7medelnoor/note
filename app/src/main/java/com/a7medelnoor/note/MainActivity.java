9024f8c4c82110cd45b9d532e8738baab547fcb3
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-09-29 01:50:22 +0400

    handle the add new note button

--- app/src/main/java/com/a7medelnoor/note/MainActivity.java
+++ app/src/main/java/com/a7medelnoor/note/MainActivity.java
@@ -1,13 +1,25 @@
 package com.a7medelnoor.note;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.a7medelnoor.note.Activity.InsertNoteActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton newNoteBtn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newNoteBtn = findViewById(R.id.addNewNoteBtn);
+        newNoteBtn.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, InsertNoteActivity.class));
+        });
     }
 }
