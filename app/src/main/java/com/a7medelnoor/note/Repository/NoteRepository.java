25d975088ab878fb38532e3fe2a4d2b5a0ae20ac
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2021-09-28 11:18:26 +0400

    create note repository and configure method insert, delete, update, select

---
+++ app/src/main/java/com/a7medelnoor/note/Repository/NoteRepository.java
@@ -0,0 +1,32 @@
+package com.a7medelnoor.note.Repository;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import com.a7medelnoor.note.Dao.NotesDao;
+import com.a7medelnoor.note.Database.NoteDatabase;
+import com.a7medelnoor.note.Model.Notes;
+
+import java.util.List;
+
+public class NoteRepository {
+    public NotesDao notesDao;
+    public LiveData<List<Notes>> getAllNotes;
+
+    public NoteRepository(Application application) {
+        NoteDatabase database = NoteDatabase.getDataBaseINSTANCE(application);
+        notesDao = database.notesDao();
+        getAllNotes = notesDao.getAllNotes();
+    }
+    void insertNote(Notes notes){
+        notesDao.insertNotes(notes);
+    }
+    void deleteNote(int id){
+        notesDao.deleteNotes(id);
+    }
+    void updateNotes(Notes notes){
+        notesDao.updateNotes(notes);
+    }
+}
